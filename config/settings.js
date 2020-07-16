4cb70bc02d76b9aaa8329f87e25891f3ca71f500
CaptainCooldev <52374169+CaptainCooldev@users.noreply.github.com>
2020-07-16 15:35:02 +0530

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,8 +1,8 @@
 module.exports = {
   'prefix': 'c.',
   'version': '1.0.0',
-  'server': 'https://discord.gg/9M5qWfy',
-  'supportID': '473426453204172811',
+  'server': 'https://discord.gg/t2UWgBU',
+  'supportID': '553256923340865567',
   'avatar': 'https://cdn.discordapp.com/attachments/685198558969856027/690727716944609331/external-content.duckduckgo.com.png',
-  'devs': ['367302593753645057', '275831434772742144'],
+  'devs': ['553256923340865567', '275831434772742144'],
 };
